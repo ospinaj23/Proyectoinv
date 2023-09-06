9f28f6a1007461ab50e3c9f6ba17d6776460c022
JOSE EDUARDO OSPINA HIGUITA <ospinahiguita@gmail.com>
2023-09-06 02:54:55 -0500

    Carga Primera Clase Basica

---
+++ Proyectoinv/Proyectoinv.Shared/Entities/Proyectoinvestigacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyectoinv.Shared.Entities
+{
+    public class Proyectoinvestigacion
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null; 
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string NameLider { get; set; }   
+        public string Descripcion { get; set; } 
+        public string Area { get; set; }
+    }
+}
