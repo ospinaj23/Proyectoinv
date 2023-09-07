773bcee02b055a6c7108a89adb03b102891856d3
JOSE EDUARDO OSPINA HIGUITA <ospinahiguita@gmail.com>
2023-09-06 22:41:30 -0500

    Se crean clases Proyectoinvestigacion e Investigador en el Shared

--- Proyectoinv/Proyectoinv.Shared/Entities/Proyectoinvestigacion.cs
+++ Proyectoinv/Proyectoinv.Shared/Entities/Proyectoinvestigacion.cs
@@ -3,17 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyectoinv.Shared.Entities
 {
     public class Proyectoinvestigacion
     {
         public int Id { get; set; }
-        public string Name { get; set; } = null; 
-        public DateTime FechaInicio { get; set; }
-        public DateTime FechaFin { get; set; }
-        public string NameLider { get; set; }   
-        public string Descripcion { get; set; } 
+
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+        
+        public DateOnly FechaInicio { get; set; }
+
+        public DateOnly FechaFin { get; set; }
+
+        [Display(Name = "NombreLider")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string NombreLider { get; set; }
+
+        [Display(Name = "Descripcion")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Area")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         public string Area { get; set; }
+    }
+
+    public class Investigador
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+
+        [Display(Name = "Afiliación")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Afiliacion { get; set; } = null;
+
+        [Display(Name = "Especialización")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Especializacion { get; set; } = null;
+
+        [Display(Name = "Rol")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Rol { get; set; } = null;
     }
 }
